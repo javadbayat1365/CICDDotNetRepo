104239fb2396e81f753a5685333d60b38e367243
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2023-02-02 13:24:43 +0330

    Init
    Testing For CICD

---
+++ CICDGitHub/CICDGitHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CICDGitHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
